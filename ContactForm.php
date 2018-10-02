4ad3c2af5c96a5e75c24b2c0b752124149e85e2e
Unknown <jhample323@west-mec.org>
2018-10-01 17:00:32 -0700

    done

--- ContactForm.php
+++ ContactForm.php
@@ -17,6 +17,7 @@
 
 <body>
     <?php
+    // Initialize global variables
     $showForm = true;
     $errorCount = 0;
     $sender = "";
@@ -24,18 +25,23 @@
     $subject = "";
     $message = "";
     
+    // Display form and errors. Values remain in input boxes
     function displayForm($sender, $email, $subject, $message){
         ?>
         <h2 style="text-align: center">Contact Me</h2>
         <form name="contact" action="ContactForm.php" method="post">
+    <!--Input box for Name-->
             <p>Your name:<br> <input type="text" name="Sender" value="<?php echo $sender; ?>"></p>
+    <!--Input box for E-mail-->
             <p>Your E-mail:<br> <input type="text" name="Email" value="<?php echo $email; ?>"></p>
+    <!--Input box for Subject-->
             <p>Subject:<br> <input type="text" name="Subject" value="<?php echo $subject; ?>"></p>
-            <p>Message:<br> 
-                <textarea name="Message"><?php echo $message?></textarea>
-            </p>
+    <!--Input box for Message-->
+            <p>Message:<br> <textarea name="Message"><?php echo $message?></textarea></p>
             <p>
+            <!--Reset form-->
                 <input type="reset" value="Clear Form">&nbsp;&nbsp;
+            <!--Submit form-->
                 <input type="submit" name="Submit" value="Send Form">
             </p>
         </form>
@@ -56,34 +62,58 @@
     }
     
     if ($showForm) {
+        // If there is an error tell the user to re-enter the info
         if($errorCount > 0){
             echo "<p>Please re-enter the form information below.</p>\n";
+            }
+        }
+        // If there are no errors inform the user the form has been submitted
+        else {
+            $senderAddress = "$sender <$email>";
+            $headers = "From:
+            $senderAddress\nCC:$senderAddress";
+            $result = mail("jaggarrr@gmail.com", $subject, $message, $headers);
+        // Message has been sent
+        if ($result){
+            echo "<p>Your message has been sent. Thank you, " . $sender . ".</p>\n";
+        }
+        // Message was not sent
+        else {
+            echo "<p>There was an error sending your message, " . $sender . ".</p>\n";
         }
     }
+    // Validate required fields
     function validateInput($data, $fieldName){
         global $errorCount;
+        // If field is empty, inform the user
         if (empty($data)){
             echo "\"$fieldName\" is a required field.<br>\n";
             ++$errorCount;
             $retval = "";
         }
+        // Else submit
         else {
             $retval = trim($data);
             $retval = stripslashes($retval);
         }
         return $retval;
     }
+    // Validate email address
+    // If missing field inform user
         function validateEmail($data, $fieldName){
-            global $errorCount;
-            if(empty($data)){
+                global $errorCount;
+                if(empty($data)){
                 echo "\"$fieldName\" is a required field.<br>\n";
                 ++$errorCount;
                 $retval = "";
             }
+            // Else validate Email
             else {
                 $retval = trim($data);
                 $retval = stripslashes($retval);
-                $pattern = "/^[\w-]+(\.[\w-]+)*@" . "(\.[a-z]{2,})$/i";
+                // Check for valid email
+                $pattern = "/^[\w-]+(\.[\w-]+)*@" . "[\w-]+(\.[\w-]+)*" . "(\.[a-z]{2,})$/i";
+                // The email is not valid
             if (preg_match($pattern, $retval) == 0){
                 echo "\"$fieldName\" is not a valid e-mail address.<br>\n";
                 ++$errorCount;
@@ -91,19 +121,6 @@
             }
             return $retval;
         }
-        displayForm($sender, $email, $subject, $message); // debug
-        else {
-        $senderAddress = "$sender <$email>";
-        $headers = "From:
-        $senderAddress\nCC:$senderAddress";
-        $result = mail("jhample323@west-mec.org", $subject, $message, $headers);
-        if ($result){
-            echo "<p>Your message has been sent. Thank you," . $sender . ".</p>\n";
-        }
-        else {
-            echo "<p>There was an error sending your message," . $sender . ".</p>\n";
-        }
-    }
     ?>
 </body>
 
--- processJumbleMarker.php
+++ processJumbleMarker.php
@@ -22,25 +22,31 @@
     $errorCount = 0;
     $words = array();
     
+    // Handles displaying errors in the form
     function displayError ($fieldName, $errorMsg) {
         global $errorCount;
         echo "Error for \"$fieldName\":$errorMsg<br>\n";
         ++$errorCount;
     }
+    // Validates the strings and names
     function validateWord ($data,$fieldName) {
         global $errorCount;
         $retval = "";
+        // Validate missing data
         if (empty($data)){
             displayError($fieldName, "This field is required");
             ++$errorCount;
             $retval = "";
-        } 
+        }
+        // Remove whitespace
+        // Displays length error
     else{
         $retval = trim($data);
         $retval = stripslashes($retval);
         if (strlen($retval) < 4 || strlen($retval) > 7) {
             displayError($fieldName, "Words must be between 4 and 7 characters in length");
         }
+        // Validates length of the words
         if (preg_match("/^[A-Za-z]+$/i", $retval) == 0) {
            displayError($fieldName, "Words must consist only of letters"); 
             }
@@ -50,10 +56,12 @@
         $retval = str_shuffle($retval);
         return $retval;
     }
+    // Retrieve data from $_POST
     $words[] = validateWord($_POST['word1'], "Word 1");
     $words[] = validateWord($_POST['word2'], "Word 2");
     $words[] = validateWord($_POST['word3'], "Word 3");
     $words[] = validateWord($_POST['word4'], "Word 4");
+    // If there are errors inform the user to fix them
     if ($errorCount > 0) {
         echo "Please use the \"Back\" button to re-enter any missing data.<br>\n";
     }
