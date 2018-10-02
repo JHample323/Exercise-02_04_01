86005078c92d667815d06d6b084d91f94ca8c3ad
Unknown <jhample323@west-mec.org>
2018-10-01 18:22:30 -0700

    Begun New Project

---
+++ Paycheck.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html>
+
+<head>
+    <!--
+    Project 02_04_02
+    Author: Jaggar Hample
+    Date: 10/01/18  
+    Filename: Paycheck.php
+    -->
+    <title>Paycheck</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0">
+    <script src="modernizr.custom.65897.js"></script>
+</head>
+
+<body>
+    <h1>Paycheck</h1>
+    <?php
+        echo "<p><a href=\"Paycheck.html\">Redo Form </a></p>\n";
+    ?>
+</body>
+
+</html>
