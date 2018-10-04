12e58df803e93e877fba48dcefadf43328b9d0fa
Unknown <jhample323@west-mec.org>
2018-10-03 18:12:23 -0700

    Work
    Worked on project

--- Paycheck.php
+++ Paycheck.php
@@ -18,27 +18,46 @@
 <body>
     <h1>Paycheck</h1>
     <?php
-
+        // declare global variables
+        $errorCount = 0;
+        $hours = "";
+        $wages = "";
+    
+        if (isset($_POST['Submit'])){
+            $hours = validateInput($_POST['hours'], "Hours");
+            $wages = validateInput($_POST['wages'], "Wages");
+            if ($errorCount == 0) {
+                $showForm = false; 
+                }
+            else{
+                $showForm = true;
+            }
+        }
+    
+        if ($showForm) {
+            // If there is an error, tell the user to resubmit the form
+            if($errorCount > 0){
+                echo "<p>Please use the link below to resubmit the form.</p>\n";
+            }
+        }
+        function validateInput($input, $fieldName){
+            global $errorCount;
+            // When the input is empty alter the user that it is empty
+            if (empty($input)){
+                 echo "\"$fieldName\" is a required field.<br>\n";
+                ++$errorCount;
+                }
+                // Check for a numeric value
+                    if (is_numeric($value)) {
+                        return $value;
+                    }
+                    else{
+                        echo "<p>The entry is not numeric. Please resubmit</p>";
+                    }
+             }
+        // hyperlink to go back to form
         echo "<p><a href=\"Paycheck.html\">Redo Form </a></p>\n";
     ?>
 </body>
 
 </html>
-
-
-<!--
-    function validateInput($data, $fieldName){
-        global $errorCount;
-        // If field is empty, inform the user
-        if (empty($data)){
-            echo "\"$fieldName\" is a required field.<br>\n";
-            ++$errorCount;
-            $retval = "";
-        }
-        // Else submit
-        else {
-            $retval = trim($data);
-            $retval = stripslashes($retval);
-        }
-        return $retval;
-    }-->
